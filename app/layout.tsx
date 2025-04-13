f8b17740bdcc6eb32bcaa1f9bcb3c231166ec666
Alejandro <smarketing.cuz@gmail.com>
2025-04-13 16:20:43 +0200

    vive coding sesh done

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,11 +26,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} relative antialiased bg-neutral-900`}
+        className={`${geistSans.variable} ${geistMono.variable} relative antialiased bg-neutral-800`}
       >
         <nav className="w-full flex items-center justify-between py-10 max-w-7xl mx-auto z-10 px-1 md:px-0">
           <Image src="/logo-white.svg" alt="logo" width={180} height={100} />
-          <Button className="cursor-pointer bg-neutral-800">Connect Wallet</Button>
+          <Button className="cursor-pointer bg-neutral-800">
+            Connect Wallet
+          </Button>
         </nav>
         {children}
       </body>
