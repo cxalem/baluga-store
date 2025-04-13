f92f8790b80e400bd6ca62423590ede66a39a6c6
Alejandro <smarketing.cuz@gmail.com>
2025-04-13 23:06:44 +0200

    Adjust BouncingImage dimensions on NoisePage for improved layout, reducing size from 15vw to 10vw and updating width and height attributes accordingly.

--- app/page.tsx
+++ app/page.tsx
@@ -89,9 +89,9 @@ export default function NoisePage() {
         <section className="relative w-full">
           <BouncingImage
             src="https://cnimmkaefpcfmcihwjhc.supabase.co/storage/v1/object/public/images//abstract-element-48.png"
-            className="absolute w-[15vw] left-[10vw] h-[15vw] top-[5vw] md:left-[20vw] z-20"
-            width={150}
-            height={150}
+            className="absolute w-[10vw] h-[10vw] left-[10vw] top-[5vw] md:left-[20vw] z-20"
+            width={120}
+            height={120}
           />
           <BouncingImage
             src="https://cnimmkaefpcfmcihwjhc.supabase.co/storage/v1/object/public/images//abstract-element-32.png"
